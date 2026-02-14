073a36263f97a85b33d56d218824f27309790378
Ayodeji Ejidiran <ayodeji.ejidiran@gmail.com>
2026-02-14 17:45:02 -0600

    Add TypeScript declarations for pdf-parse

---
+++ types/pdf-parse.d.ts
@@ -0,0 +1,13 @@
+declare module "pdf-parse" {
+  type PdfParseResult = {
+    text: string;
+    numpages: number;
+    numrender: number;
+    info?: Record<string, unknown>;
+    metadata?: unknown;
+    version?: string;
+  };
+
+  function pdfParse(dataBuffer: Buffer, options?: Record<string, unknown>): Promise<PdfParseResult>;
+  export default pdfParse;
+}
